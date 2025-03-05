b5ea9111f6b27a2a08f531c5f1ff988e811f8144
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-03-05 23:44:24 +0530

    DSA with javascript

---
+++ Sherianys DSA with javascript/master of basics/01swaping.js
@@ -0,0 +1,31 @@
+// stape 1
+// let a = 10;
+// let b = 20;
+
+// let c;
+
+// c = a // 10
+// a = b // 20
+// b = c // 10
+
+// console.log(a)
+// console.log(b)
+
+// stape 2
+
+// let a = 10
+// let b = 20
+
+// a = a + b // 30
+// b = a - b // 10
+// a = a - b //  20
+// console.log(a);
+// console.log(b)
+
+// stape 3 
+
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+console.log(a, b);
---
+++ Sherianys DSA with javascript/master of basics/02poerators.js
@@ -0,0 +1,34 @@
+// + - * / % 
+
+// console.log(7%2);
+
+// < > <= >= !== relational operators 
+// console.log(10 == 10); // true
+
+
+// && ||  logical operator 
+// && : all condactions is true then op is true
+// || : one condaction is true then op is true 
+
+// console.log(10 > 5 && 15 < 12); // false
+// console.log(10 > 5 || 15 < 12); // true 
+
+// unary operator ++ --
+let i = 11
+console.log(i);
+
+i = i++ + ++i 
+console.log(i);
+
+// solve 
+let a = 11;   
+let b = 22;
+
+let c = a + b + a++ + b++ + ++a + ++b;
+console.log("a = " + a) //   
+console.log("b = " + b) //   
+console.log("c = " + c) //  
+
+let z = 33
+z = z++
+console.log(z)
