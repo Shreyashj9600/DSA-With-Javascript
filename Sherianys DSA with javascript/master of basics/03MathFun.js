0a0f0a005e75b8a3642f465e85a0fccf55b824de
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-03-06 15:22:03 +0530

    Match function

---
+++ Sherianys DSA with javascript/master of basics/03MathFun.js
@@ -0,0 +1,15 @@
+console.log(Math.round(10.6));
+console.log(Math.random());
+console.log(Math.trunc(Math.random()*9000 + 1000));
+console.log(Math.floor(3.9));
+
+// Q. calculate the area and parameter of reactange
+
+let a = 5
+let b = 7
+console.log(a*b);
+
+// circumference of circel
+
+let r = 12
+console.log(Number((2*Math.PI*r).toFixed(2)));
