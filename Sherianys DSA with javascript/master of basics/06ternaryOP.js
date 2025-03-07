0948a1b8c6baecc0229926a990396d78a8314ddf
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-03-07 23:03:36 +0530

    ternary operator

---
+++ Sherianys DSA with javascript/master of basics/06ternaryOP.js
@@ -0,0 +1,8 @@
+// ternary operator 
+
+112 > 13 ? console.log("yes") : console.log("no");
+console.log(12 > 13 ? "yes" : "no");
+
+// nested ternary operator
+let num = -12
+console.log(num > 0 ? "positive" : num < 0 ? "negative" : "zero");
