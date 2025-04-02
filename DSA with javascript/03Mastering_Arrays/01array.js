ef5bd5c584c9ad8fca8e4d57158ac9eb9151d519
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-04-02 15:06:50 +0530

    basic of arrays

--- DSA with javascript/03Mastering_Arrays/01array.js
+++ DSA with javascript/03Mastering_Arrays/01array.js
@@ -14,7 +14,7 @@
 // console.log(arr);
 
 //--------------------------------------
-// addading array element 
+// addation of array all element 
 
 // let arr = [10, 20, 30, 40, 50]
 
@@ -41,18 +41,58 @@
 // let maxNumber = Math.max(...arr)
 // console.log("maxNumber", maxNumber);
 
-
 // how to find second largest number in give arr 
 
-let myArr = [10, 30, 56, 43, 29, 64, 49, 60];
+// let myArr = [10, 30, 40, 40, 40, 40, 40];
+// //                       i 
+// let max = Math.max(myArr[0], myArr[1])  // 40
+// let sMax = Math.min(myArr[0], myArr[1]) // 30                       
 
-let max = Math.max(myArr[0] , myArr[1])
-let sMax = Math.min(myArr[0] , myArr[1])
+// for (let i = 2; i < myArr.length; i++) {
+//     if (myArr[i] > max) {
+//         sMax = max
+//         max = myArr[i]
+//     }
+//     else if (myArr[i] > sMax && max != myArr[i]) {
+//         sMax = myArr[i]
+//     }
+// }
+// console.log(sMax);
 
-for(let i = 2; i < myArr.length; i++) {
-    if(myArr[i] > max){
-        max = myArr[i]
-        sMax = max
+// Revercie the array
+
+// let arr = [10, 20, 30, 40, 50]
+
+// let i = 0, j = arr.length - 1;
+
+// while (i != j) {
+//     let temp = arr[i]
+//     arr[i] = arr[j]
+//     arr[j] = temp
+//     i++
+//     j--
+// }
+// console.log(arr);
+
+
+// for (let i = arr.length-1; i >= 0; i--) {
+//     console.log(arr[i])
+// }
+
+// swaping i and j positions 
+
+let arr = [1, 1, 0, 1, 0, 1, 1, 0, 0]
+
+let i = 0, j = 0
+
+while (i < arr.length) {
+    if (arr[i] == 0) {
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        j++
     }
+    i++
 }
 
+console.log(arr);
